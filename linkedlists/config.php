51fa2272e7b6cf8795ce0775c7c7a03977ff320c
Marian Dörk <doerk@fh-potsdam.de>
2020-10-07 22:14:55 +0200

    lod3

---
+++ linkedlists/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$data_url = "https://gist.githubusercontent.com/alliyya/d43b8cd389eec3e6306f35eac5733076/raw/fc9b897f93a59e7267d8e9be17bc1f8d8a7f38ad/clode_data.csv"; // leave empty if local data.csv is used
+
+$website_title = "Title";
+$website_url = "https://mariandoerk.de/linkedlists/globalvoics/"; // with trailing slash
+
+$website_description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+$about_section = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+$contact_details ='Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est';
+
+// should match column names in data.csv
+$facet_left = "keywords";
+$facet_right = "collaborators";
+$facet_middle = "";
+
+// should be used in type column
+$types = array("Events", "Projects", "Publications");
+
+?>
