d10694eb86a1a2ca6557c86f69feb7ba89b1e8d1
Marian Dörk <doerk@fh-potsdam.de>
2020-10-20 20:08:01 +0200

    new about section

--- linkedlists/config.php
+++ linkedlists/config.php
@@ -8,18 +8,19 @@ $website_url = "https://mariandoerk.de/linkedlists/globalvoics/"; // with traili
 
 $website_description = "Recent activity related to linked open cultural data, with a focus on Canadian initiatives and diversity matters over the past 5 years.";
 
-$about_section = 'This is a work-in-progress and we want it to grow: <a href="https://forms.gle/kgw3B4B5oKLpovwRA">suggest an addition</a>.<br>
-This beta of the Linked Open Lists tool emerges from collaboration between the <a href="https://uclab.fh-potsdam.de/">Urban Complexity Lab</a> and <a href="http://lincsproject.ca/">LINCS</a>, which will extend the <a href="https://github.com/nrchtct/linkedlists_lod3">code</a> to work with linked data.';
+$about_section = 'Recent activity related to linked open cultural data, with a focus on Canadian initiatives and diversity matters over the past 5 years. This is a work-in-progress and we want it to grow: <a href="https://forms.gle/kgw3B4B5oKLpovwRA">suggest an addition</a>.';
 
 $contact_details ='
-<a href="https://lincsproject.ca/">lincsproject.ca</a><br>
-<a id="email" href="mailto:lincs@uoguelph.ca?subject=Linked%20List%20Visualization">lincs@uoguelph.ca</a><br>
-<a href="https://twitter.com/lincsproject">@lincsproject</a>';
+This version of the <a href="https://github.com/nrchtct/linkedlists">Linked Lists</a> tool emerges from a collaboration between the <a href="https://uclab.fh-potsdam.de/">UCLAB</a> and <a href="http://lincsproject.ca/">LINCS</a>, which will extend the <a href="https://github.com/nrchtct/linkedlists_lod3">code</a> to work with linked data.<br><br>		
+	
+Connect with LINCS: <a href="https://lincsproject.ca/">Web</a> · 
+<a id="email" href="mailto:lincs@uoguelph.ca?subject=Linked%20List%20Visualization">Email</a> · 
+<a href="https://twitter.com/lincsproject">Twitter</a>';
 
 // should match column names in data.csv
-$facet_left = "keywords";
-$facet_right = "organizations";
+$facet_left = "organizations";
 $facet_middle = "collaborators";
+$facet_right = "keywords";
 
 // should be used in type column
 $types = array("Events", "Projects", "Publications");
